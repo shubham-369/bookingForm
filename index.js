86cad99b41f10d7b7d7ea81711448601bdc6194b
shubham srivastav <112486333+shubham-369@users.noreply.github.com>
2023-06-13 19:42:21 +0530

    updated form deleting functionality

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function validateEmail(email) {
   }
 function validateName(name) {
     // Regular expression pattern for name validation
-    var namePattern = /^[a-zA-Z]+ [a-zA-Z]+$/;
+    var namePattern = /^[a-zA-Z]+\d+$/;
     var nam = name.trim()
     // Test the name against the pattern
     return namePattern.test(nam);
@@ -15,22 +15,21 @@ function validateName(name) {
 var u_name = document.getElementById('name');
 u_name.addEventListener("input",function(){
     if(!validateName(u_name.value)){
-        document.getElementById("name").nextSibling.textContent="Incorrect Format!";
+        document.getElementById("name").nextElementSibling.style.display="block";
     }
     else{
-        document.getElementById("email").nextSibling.textContent="";
+        document.getElementById("name").nextElementSibling.style.display="none";
     }
 });
 var email = document.getElementById('email');
 email.addEventListener("input",function(){
     if(!validateEmail(email.value)){
-        document.getElementById("email").nextSibling.textContent="Invalid Email!";
+        document.getElementById("email").nextElementSibling.style.display="block";
     }
     else{
-        document.getElementById("email").nextSibling.textContent="";
+        document.getElementById("email").nextElementSibling.style.display="none";
     }
 });
-
 const form = document.getElementById("Form")
 document.getElementById("Form").addEventListener("submit", function(event) {
     event.preventDefault();
@@ -39,23 +38,41 @@ var phone = document.getElementById('phone').value;
 var date = document.getElementById('date').value;
 var time = document.getElementById('time').value;
 
-var user_details = {
-    name:u_name.value,
-    phone:phone,
-    email:email.value,
-    date:date,
-    time:time
+
+// Create a new user object
+const newUser = {
+  name: u_name.value,
+  phone: phone,
+  email: email.value,
+  date: date,
+  time: time
 };
-localStorage.setItem('user_details',JSON.stringify(user_details));
 
-console.log('Name : ', u_name.value);
-console.log('Phone : ', phone);
-console.log('Email : ', email.value);
-console.log('Date : ', date);
-console.log('Time : ', time);
+// Convert the updated data array to a string
+const updatedDataString = JSON.stringify(newUser);
+// Store the updated string in local storage
+localStorage.setItem(newUser.email, updatedDataString);
 
-form.reset()
 
-document.getElementById("submitted").style.display="flex";
+form.reset();
+
+let i = localStorage.length-1;
+const div = document.getElementById("user-data");
+let key = localStorage.key(i);
+let value = localStorage.getItem(key);
+let obj = JSON.parse(value);
+let li = document.createElement("li");
+li.innerHTML = `${obj.name} ${obj.phone} ${obj.email} ${obj.date} ${obj.time} <button class='delete' data-email="${obj.email}">Delete</button><br><br>`;
+div.querySelector('ul').appendChild(li);
+
+
+let del = document.getElementById('user-data').querySelector('ul');
+del.addEventListener('click',(e)=>{
+    if(e.target.classList.contains('delete')){
+        let key = e.target.getAttribute('data-email');
+        e.target.parentElement.remove();
+        localStorage.removeItem(key);        
+    }
+});
 
 });
