4be660b56c10fe7dd5ac009fc70aa1b3ab586bdc
shubham srivastav <112486333+shubham-369@users.noreply.github.com>
2023-08-19 16:19:51 +0530

    getting data to form using frontend

--- public/main.js
+++ public/main.js
@@ -1,3 +1,5 @@
+
+const fid = document.getElementById('id');
 const u_name = document.getElementById('name');
 const phone = document.getElementById('phone');
 const email = document.getElementById('email');
@@ -23,14 +25,13 @@ form.addEventListener("submit", async (e) => {
 
     const div = document.getElementById("user-data");
     div.querySelector('ul').innerHTML = '';
-    axios.get('http://localhost:3333/index')
+    axios.get('http://localhost:3333/form')
         .then((response) => {
             const obj = response.data;
-            console.log(obj);
             for(let i=0; i<obj.length; i++){
                 let li = document.createElement("li");
                 li.style.color = 'black';
-                li.innerHTML = `${obj[i].name} - ${obj[i].phone} - ${obj[i].email} <a class='btn btn-danger' href="delete/${obj[i].id}">Delete</a>&nbsp;<a class='btn btn-secondary' href="edit/${obj[i].id}">Edit</a><br><br>`;
+                li.innerHTML = `${obj[i].name} - ${obj[i].phone} - ${obj[i].email} <a class='btn btn-danger' href="delete/${obj[i].id}">Delete</a>&nbsp;<button class='btn btn-secondary edit' data-id="${obj[i].id}">Edit</button><br><br>`;
                 div.querySelector('ul').appendChild(li);
             }
         })
@@ -40,17 +41,34 @@ form.addEventListener("submit", async (e) => {
 });
 
 const div = document.getElementById("user-data");
-axios.get('http://localhost:3333/index')
+axios.get('http://localhost:3333/form')
     .then((response) => {
         const obj = response.data;
         for(let i=0; i<obj.length; i++){
             let li = document.createElement("li");
             li.style.color = 'black';
-            li.innerHTML = `${obj[i].name} - ${obj[i].phone} - ${obj[i].email} <a class='btn btn-danger' href="delete/${obj[i].id}">Delete</a>&nbsp;<a class='btn btn-secondary' href="edit/${obj[i].id}">Edit</a><br><br>`;
+            li.innerHTML = `${obj[i].name} - ${obj[i].phone} - ${obj[i].email} <a class='btn btn-danger' href="delete/${obj[i].id}">Delete</a>&nbsp;<button class='btn btn-secondary edit' data-id="${obj[i].id}">Edit</button><br><br>`;
             div.querySelector('ul').appendChild(li);
         }
     })
     .catch((error) => console.log('error while fetching data : ', error));
 
-axios.get('http://localhost:3333/edit')
+function listdata(list){
+    const arr = list.split('-');
+    const l = arr[arr.length-1];
+    arr.pop();
+    const categ = l.split(' ');
+    arr.push(categ[1]); 
+    return arr;
+}
 
+div.addEventListener('click', (e) => {
+    if(e.target.classList.contains('edit')){
+        const id = e.target.getAttribute('data-id');
+        const text = e.target.parentElement.textContent;
+        fid.value = id;
+        u_name.value = listdata(text)[0];
+        phone.value = parseInt(listdata(text)[1]);
+        email.value = listdata(text)[2];
+    }
+});
